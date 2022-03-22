d83e29d7fe6548c93a762d0cbab5e7f9747b2101
Veerle <veerle.wilms@mpi.nl>
2022-03-22 16:12:58 +0100

    add guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,19 +1,20 @@
 from random import randint  # Import the randint function from the random package
 # import random
 
-number = randint(1, 10)  # Generate a random number between 1 and 10
+number = randint(1, 100)  # Generate a random number between 1 and 100
 print(number)  # Print the number (to make testing easier)
 
 # Ask the user for a guess, and convert the result to an integer
-guess = int(input('Guess the number (between 1 and 10): '))
+guess = int(input('Guess the number (between 1 and 100): '))
 
-# While the correct number was not guessed...
 while number != guess:
-    print('Incorrect...')  # Let the user know that their answer was incorrect
-    guess = int(input('Try again: '))  # And ask them to guess again
-
-# When the above while loop terminates, that means the correct number was guessed!
-print('Correct!')
+    if guess < number:
+        print("Guess is too low!")
+        guess = int(input('Try again: '))
+    elif guess > number:
+        print("Guess is too high!")  
+        guess = int(input('Try again: '))
+print("Correct!")
 
 # Homework: extend the above program so that:
 #   1) it generates a number between 1 and 100,
